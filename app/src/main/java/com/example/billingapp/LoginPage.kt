f5897b2b92a89eaa3c0503e61976f7606efdfeba
DivineJMd <adarsh21442@iiitd.ac.in>
2023-10-04 15:50:27 +0530

    first commit

--- app/src/main/java/com/example/billingapp/LoginPage.kt
+++ app/src/main/java/com/example/billingapp/LoginPage.kt
@@ -2,9 +2,14 @@
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,10 +17,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.Wallpapers
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.billingapp.ui.theme.BillingAppTheme
+
 
 class LoginPage {
     @Composable
@@ -28,27 +36,56 @@ class LoginPage {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = "Login Screen",
-                style = TextStyle(fontSize = 28.sp, color = Color.Black)
-            )
-
-            // Add a Button with custom background color
             Button(
-                onClick = {
-                    // Handle button click action here
-                    // For example, you can navigate to another screen
-                },
-                modifier = Modifier.padding(16.dp),
-
+                onClick = { /* Handle button click action here */ },
+                modifier = Modifier
+                    .padding(10.dp)
+                    .height(50.dp)
+                    .width(500.dp),
+                colors = ButtonDefaults.buttonColors(
+                    Color(0xFFFF6B00)
+                ),
+                shape = RoundedCornerShape(20.dp)
             ) {
-                Text(text = "Login")
+                Text(text = "Login as Admin",
+                    style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp,
+                        color = Color.White
+                    )
+                )
+            }
+
+            Spacer(modifier = Modifier.height(16.dp)) // Adjust the height as needed
+
+            Button(
+                onClick = { /* Handle button click action here */ },
+                modifier = Modifier
+                    .padding(10.dp)
+                    .height(50.dp)
+                    .width(500.dp),
+                colors = ButtonDefaults.buttonColors(
+                    Color(0xFFFF6B00)
+                ),
+                shape = RoundedCornerShape(20.dp)
+            ) {
+                Text(
+                    text = "Login as User",
+                    style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp,
+                        color = Color.White
+                    )
+                )
             }
         }
     }
 }
 
+
 @Preview
+    (showBackground = true,
+    wallpaper = Wallpapers.YELLOW_DOMINATED_EXAMPLE)
 @Composable
 fun LoginPagePreview() {
     BillingAppTheme {
--- app/src/main/java/com/example/billingapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/billingapp/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.example.billingapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Purple80 = Color(0xFFFF6F00)
+val PurpleGrey80 = Color(0xFFDB6A00)
+val Pink80 = Color(0xFFC45F00)
 
-val Purple40 = Color(0xFFFF6B00)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Purple40 = Color(0xFFFF8B10)
+val PurpleGrey40 = Color(0xFFDB6A00)
+val Pink40 = Color(0xFFFDA172)
